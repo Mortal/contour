294a55e3588cc691c98a9d49cb7c25ca8ffef2da
Mathias Rav <rav@cs.au.dk>
2017-07-19 11:12:41 +0200

    Add Bezier3

--- spline.py
+++ spline.py
@@ -49,3 +49,45 @@ class BezierN:
         t = t.reshape((t.size,) + self._output_shape)
         c = self.c.reshape((-1,) + (1,)*t.ndim + self.c.shape[1:])
         return self._eval(0, len(c), t, c).reshape(output_shape)
+
+
+class Bezier3:
+    '''
+    >>> c = [0, 9, 15, 0]
+    >>> print(BezierN(c)(0.314159) - Bezier3(c)(0.314159))
+    0.0
+    >>> c2 = [[0, 0], [9, 0], [15, 14], [0, 3]]
+    >>> t = np.linspace(0, 1)
+    >>> np.allclose(BezierN(c2)(t), Bezier3(c2)(t))
+    True
+    '''
+
+    _output_shape = (-1,)
+
+    def __init__(self, control_points):
+        control_points = np.asarray(control_points)
+        if control_points.ndim == 2:
+            self.c = control_points
+        elif control_points.ndim == 1:
+            self._output_shape = ()
+            self.c = control_points.reshape(-1, 1)
+        else:
+            raise ValueError('invalid number of dimensions (expected 1 or 2)')
+        if self.c.shape[0] != 4:
+            raise ValueError('cubic bezier requires 4 control points')
+
+    def __call__(self, t):
+        t = np.asarray(t)
+        output_shape = t.shape + self._output_shape
+        t = t.reshape((t.size,) + self._output_shape)
+
+        r = 1-t
+        p0, p1, p2, p3 = self.c.reshape((4, 1) + self._output_shape)
+        c01 = r * p0 + t * p1
+        c12 = r * p1 + t * p2
+        c23 = r * p2 + t * p3
+        c02 = r * c01 + t * c12
+        c13 = r * c12 + t * c23
+        c03 = r * c02 + t * c13
+
+        return c03.reshape(output_shape)
